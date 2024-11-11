09b7e712d701a35a6ef6436600071d07a5495aa2
Julia Viger <144358015+juliavig@users.noreply.github.com>
2024-11-10 19:01:10 -0500

    Add files via upload

---
+++ Lab1/HelloWorld.java
@@ -0,0 +1,8 @@
+package package1;
+public class HelloWorld {
+public static void main (String[] args) {
+// This is a comment line
+// This line output to the screen
+System.out.println("Hello My friends in ELEC 279");
+}
+}
---
+++ Lab1/Navigator.java
@@ -0,0 +1,7 @@
+package package1;
+public class Navigator {
+	public static void main(String[] args) {
+		System.out.println("The Navigator is now the Driver");
+	}
+	
+}
